0b5b176fbc4d21b268bcdb7cf58a164c86015b92
Tasharah Person <typerson@wpi.edu>
2018-11-11 18:40:49 -0500

    Add three buttons to home screen with task completed prompt

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import {NavController } from 'ionic-angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+
+    constructor(public navCtrl: NavController) {
+
+    }
+
+    btnClicked(){
+        alert("Task Completed");
+    }
+}
