2ca0596c11d8d2e247ffc7feec5937d627c4781e
Pawan Dodani <pdodani@restandrenew.wpi.edu>
2018-11-11 23:00:39 -0500

    calendar commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,16 @@
-import { IonicModule } from '@ionic/angular';
+import { IonicApp,IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import {NgCalendarModule } from 'ionic2-calendar';
+import { MyApp } from './app/app.component';
 
 @NgModule({
   imports: [
-    IonicModule,
+    NgCalendarModule,
+    IonicModule.forRoot(HomePage),
     CommonModule,
     FormsModule,
     RouterModule.forChild([{ path: '', component: HomePage }])
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import {navController} from 'ionic-angular';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+  eventSource = [];
+  viewTtile: string;
+  selectedDay = new Date();
+
+  calendar = {
+    mode: 'month',
+    currentDate: this.selectedDay
+}
+    constructor(public navCtrl: navController) {
+
+  }
+  onViewTitleChanged(title){
+  this.viewTitle = title;
+  }
+
+  }
+  onTimeSelected(event){
+  this.selectedDay = ev.selectedTime;
+
+  }
+  onEventSelected(event){
+}
+
+
+
