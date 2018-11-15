b3cdb1a1b283491df31fee38394fdc8bf7c4911b
Rocky Santos <rockysantos@extranet-cntr-130-215-176-251.vpn.wpi.net>
2018-11-15 18:33:25 -0500

    add profile page to tabs

--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -5,6 +5,7 @@ import { TabsPage } from './tabs.page';
 import { HomePage } from '../home/home.page';
 import { AboutPage } from '../about/about.page';
 import { ContactPage } from '../contact/contact.page';
+import { ProfilePage } from '../profile/profile.page';
 
 const routes: Routes = [
   {
@@ -30,7 +31,12 @@ const routes: Routes = [
         path: 'contact',
         outlet: 'contact',
         component: ContactPage
-      }
+      },
+        {
+            path: 'profile',
+            outlet: 'profile',
+            component: ProfilePage
+        }
     ]
   },
   {
