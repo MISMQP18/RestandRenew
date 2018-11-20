db7cc0d8d28c7b51252a42da425b0fa4859417ff
Rocky Santos <rockysantos@extranet-cntr-130-215-176-190.vpn.wpi.net>
2018-11-20 10:38:55 -0500

    remove profile from app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'Profile', loadChildren: './profile/profile.module#ProfilePageModule' },
   { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule' }
 ];
 @NgModule({
