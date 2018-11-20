d2a9385afbbcc3f513175ccc33d3b8f375e052ea
Rocky Santos <rockysantos@Rocky-MacBook.local>
2018-11-20 11:53:23 -0500

    add settings path

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -1,15 +1,14 @@
+import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
-import { IonicModule } from '@ionic/angular';
-
 import { SettingsPage } from './settings.page';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'settings',
     component: SettingsPage
   }
 ];
