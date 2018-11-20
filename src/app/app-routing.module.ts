d243aaa3b12af3e1d91682613a309a23b34a66c4
Rocky <rocky.santos4@gmail.com>
2018-11-20 15:27:33 +0000

    add habit settings page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'Profile', loadChildren: './profile/profile.module#ProfilePageModule' }
+  { path: 'Profile', loadChildren: './profile/profile.module#ProfilePageModule' },
+  { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
