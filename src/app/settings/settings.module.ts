20ae43586cf5aae6725ef9d51ce6218152a20301
Rocky Santos <rockysantos@Rocky-MacBook.fios-router.home>
2018-11-20 12:30:32 -0500

    edit settings path

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -1,24 +1,18 @@
 import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
 
 import { SettingsPage } from './settings.page';
 
-const routes: Routes = [
-  {
-    path: 'settings',
-    component: SettingsPage
-  }
-];
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild([{ path: '', component: SettingsPage }])
   ],
   declarations: [SettingsPage]
 })
