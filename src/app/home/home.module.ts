ff7586ef48db4abde94e8942a337e8700ade6b00
j11santos <rocky.santos4@gmail.com>
2018-11-26 16:41:25 -0500

    remove pawan calendar attempt 3000

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,12 +4,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-import { CalendarModule} from 'ionic3-calendar-en';
+//import { CalendarModule} from 'ionic3-calendar-en';
 
 @NgModule({
     imports: [
         IonicModule,
-        CalendarModule,
+        //CalendarModule,
         CommonModule,
         FormsModule,
         RouterModule.forChild([{ path: '', component: HomePage }])
