22ac58c4cfdce22516e58f959941fda4c48eb018
Pawan Dodani <pdodani@restandrenew.wpi.edu>
2018-11-25 23:57:48 -0500

    calendar attempt 1

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import { CalendarModule} from 'ionic3-calendar-en';
 
 @NgModule({
     imports: [
         IonicModule,
+        CalendarModule,
         CommonModule,
         FormsModule,
         RouterModule.forChild([{ path: '', component: HomePage }])
