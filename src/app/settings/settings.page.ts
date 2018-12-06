ac7c3d740c8cb2bfc033be66edfc73d6bd3c2b46
j11santos <rocky.santos4@gmail.com>
2018-12-06 11:56:10 -0500

    remove ion items from settings and use one item per list divider

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -8,16 +8,22 @@ import { reorderArray } from 'ionic-angular';
   styleUrls: ['settings.page.scss'],
 })
 export class SettingsPage {
-    items = [];
+    items1 = ['Drink Water', 'Meditate at lunchtime'];
+    items2 = ['Take 3 deep breaths', 'Take a walk', 'Get up from your desk'];
 
     constructor() {
         for (let x = 0; x < 5; x++) {
-            this.items.push(x);
+            this.items1.push(x);
+        }
+
+        for (let x = 0; x < 5; x++) {
+            this.items2.push(x);
         }
     }
 
     reorderItems(indexes) {
-        this.items = reorderArray(this.items, indexes);
+        this.items1 = reorderArray(this.items1, indexes);
+        this.items2 = reorderArray(this.items2, indexes);
     }
 
     btnClicked() {
@@ -25,3 +31,30 @@ export class SettingsPage {
     }
 
 }
+
+/*
+
+<ion-item>
+        <ion-label>Drink Water</ion-label>
+        <ion-toggle checked="true"></ion-toggle>
+      </ion-item>
+      <ion-item>
+        <ion-label>Meditate at lunchtime</ion-label>
+        <ion-toggle checked="true"></ion-toggle>
+      </ion-item>
+*/
+
+/*
+<ion-item>
+      <ion-label>Take 3 deep breaths</ion-label>
+      <ion-toggle></ion-toggle>
+    </ion-item>
+    <ion-item>
+      <ion-label>Take a walk</ion-label>
+      <ion-toggle></ion-toggle>
+    </ion-item>
+    <ion-item>
+      <ion-label>Get up from your desk</ion-label>
+      <ion-toggle></ion-toggle>
+    </ion-item>
+ */
