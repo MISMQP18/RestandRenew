57e654de306f323cfb29267ddc3c3d23d597945c
meghanflynn <msflynn@wpi.edu>
2018-12-10 10:43:26 -0500

    connect firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,36 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IonicStorageModule } from '@ionic/storage'
+import { IonicStorageModule } from '@ionic/storage';
+
+import { HttpModule } from '@angular/http';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import { FirebaseProvider } from './../providers/firebase/firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC-_TjGZ3Sl_JYWSg3uag4IyiQHhIH8Yds",
+    authDomain: "rest-and-renew.firebaseapp.com",
+    databaseURL: "https://rest-and-renew.firebaseio.com",
+    projectId: "rest-and-renew",
+    storageBucket: "rest-and-renew.appspot.com",
+    messagingSenderId: "888747261921"
+};
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
+  imports: [BrowserModule,
+      IonicModule.forRoot(),
+      AppRoutingModule,
+      IonicStorageModule.forRoot(),
+      HttpModule,
+      AngularFireDatabaseModule,
+      AngularFireModule.initializeApp(firebaseConfig),],
   providers: [
     StatusBar,
     SplashScreen,
+      FirebaseProvider,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
