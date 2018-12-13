86b1ff3149e30c0db87c9553ea430a61d4410ec4
j11santos <rocky.santos4@gmail.com>
2018-12-12 21:18:13 -0500

    attempt to add color to summary page 1

--- src/app/summary/summary.page.ts
+++ src/app/summary/summary.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from '../services/authentication.service';
 
 @Component({
   selector: 'app-summary',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SummaryPage implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) { }
 
   ngOnInit() {
   }
+
+    logout() {
+        this.authService.logout();
+    }
 
 }
