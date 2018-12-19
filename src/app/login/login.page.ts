03be9c2bfcc85f7200dd2cf75ceea185bad86bd4
j11santos <rocky.santos4@gmail.com>
2018-12-19 21:21:11 +0700

    fix login images and remove firebase attempt 1

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+    imgSrc = 'https://www.wpi.edu/sites/default/files/inline-image/Offices/Marketing-Communications/WPI_Inst_Prim_FulClr.png';
+    image = 'https://reliantmedicalgroup.org/wp-content/themes/premise/img/reliant-brand-2016-small.png?x20341';
+
+
     constructor(private authService: AuthenticationService) { }
 
     ngOnInit() {
