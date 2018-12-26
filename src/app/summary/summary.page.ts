c36443689525e514c05da0868c25821706e6de0e
meghanflynn <msflynn@wpi.edu>
2018-12-26 12:15:47 -0500

    templates for summary graphics (not finished)

--- src/app/summary/summary.page.ts
+++ src/app/summary/summary.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {AuthenticationService} from '../services/authentication.service';
 import { Router } from '@angular/router';
+import { Chart } from 'chart.js';
+import {NavController} from "@ionic/angular";
 
 @Component({
   selector: 'app-summary',
@@ -9,17 +11,65 @@ import { Router } from '@angular/router';
 })
 export class SummaryPage implements OnInit {
 
-  constructor(private authService: AuthenticationService, private router: Router) { }
+    @ViewChild('barCanvas') barCanvas;
 
-  ngOnInit() {
-  }
+    barChart: any;
 
-  logout() {
-    this.authService.logout();
-  }
+    constructor(private authService: AuthenticationService, private router: Router, public navCtrl: NavController) {
+    }
 
-  openHabitSettingsPage(){
-    this.router.navigateByUrl('/tabs/(settings:settings)');
-  }
+    ngOnInit() {
+    }
 
+    logout() {
+        this.authService.logout();
+    }
+
+    openHabitSettingsPage() {
+        this.router.navigateByUrl('/tabs/(settings:settings)');
+    }
+
+   /* ionViewDidLoad() {
+
+        this.barChart = new Chart(this.barCanvas.nativeElement, {
+
+            type: 'bar',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+
+        });
+    }*/
 }
+
+
