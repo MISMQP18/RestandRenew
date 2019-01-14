a5cc6a923f46ae2d32e5437b0a4a449fe36c1f13
j11santos <rocky.santos4@gmail.com>
2019-01-14 14:18:55 -0500

    set up firebase hosting for testing purposes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+    firebase:{
+        apiKey: "AIzaSyC-_TjGZ3Sl_JYWSg3uag4IyiQHhIH8Yds",
+        authDomain: "rest-and-renew.firebaseapp.com",
+        databaseURL: "https://rest-and-renew.firebaseio.com",
+        projectId: "rest-and-renew",
+        storageBucket: "rest-and-renew.appspot.com",
+        messagingSenderId: "888747261921"
+    }
 };
